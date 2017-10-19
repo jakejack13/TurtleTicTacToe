bcf3606522a86a89c89e1798fda37d1b4c346f9f
jakejack13 <jckawesome13@gmail.com>
2017-10-19 18:49:56 -0400

    Finished square picking and placement

--- KerrJacobTicTacToe.py
+++ KerrJacobTicTacToe.py
@@ -2,22 +2,29 @@
 #Tic-Tac-Toe
 
 #Database of Coordinates
-Coor1_x = 100
-Coor1_y = -100
-Coor2_x = False
-Coor2_y = False
-Coor3_x = False
-Coor3_y = False
-Coor4_x = False
-Coor4_y = False
-Coor5_x = False
-Coor5_y = False
-Coor6_x = False
-Coor6_y = False
-Coor7_x = False
-Coor7_y = False
-Coor8_x = False
-Coor8_y = False
+CoorX = []
+CoorY = []
+for i in range(10):
+        CoorX.append(1)
+        CoorY.append(1)
+CoorX[1] = 50
+CoorY[1] = 250
+CoorX[2] = 150
+CoorY[2] = 250
+CoorX[3] = 250
+CoorY[3] = 250
+CoorX[4] = 50
+CoorY[4] = 150
+CoorX[5] = 150
+CoorY[5] = 150
+CoorX[6] = 250
+CoorY[6] = 150
+CoorX[7] = 50
+CoorY[7] = 50
+CoorX[8] = 150
+CoorY[8] = 50
+CoorX[9] = 250
+CoorY[9] = 50
 
 import turtle
 turtle.hideturtle()
@@ -26,7 +33,6 @@ turtle.goto(300, 0)
 turtle.goto(300, 300)
 turtle.goto(0, 300)
 turtle.goto(0, 0)
-
 #Make grid
 turtle.goto(0,100)
 turtle.goto(300,100)
@@ -42,11 +48,27 @@ turtle.goto(200,300)
 #First time playthrough set up
 PlayerTurn = 0
 PlayState = 1
-
 #Play Loop
-for PlayState in range(1)
-TurnSelection = int(input("Please select the number of the square to play your tile: "))
-turtle.goto(Coor[TurnSelection]_x,Coor[TurnSelection]_y)
-if PlayerTurn == 0 :
-	turtle.write("X")
-#Continue X playthrough instructions
+while(PlayState == 1):
+        if PlayerTurn == 0 :
+                print("Player X's turn")
+        elif PlayerTurn == 1 :
+                print("Player O's turn")
+        else:
+                print("Error! Shutting down program!")
+                break
+        TurnSelection = int(input("Please select the number of the square to play your tile: "))
+        if TurnSelection > 9 or TurnSelection < 1 :
+                print("That's not a square. ERROR SHUTTING DOWN PROGRAM!")
+                break
+        turtle.penup()
+        turtle.goto(CoorX[TurnSelection],CoorY[TurnSelection])
+        turtle.pendown()
+        if PlayerTurn == 0 :
+                turtle.write("X", font=("Arial", 30, "normal"),align="center")
+                PlayerTurn = 1
+        elif PlayerTurn == 1 :
+                turtle.write("O", font=("Arial", 30, "normal"),align="center")
+                PlayerTurn = 0
+        else :
+                print("Error! Shutting down program!")
