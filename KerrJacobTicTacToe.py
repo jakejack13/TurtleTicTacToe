92a7c9df9d976e7a54ff49338bd2ed8615710b37
jakejack13 <jckawesome13@gmail.com>
2017-11-29 20:49:05 -0500

    Slight text fixes

--- KerrJacobTicTacToe.py
+++ KerrJacobTicTacToe.py
@@ -10,8 +10,8 @@ square_state = []
 #playerSelections
 playerSelections = []
 player0Selections = []
-player1Selections = []  #the input
-winningCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]  #the list of correct solutions
+player1Selections = [] #the input
+winningCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]] #the list of correct solutions
 
 for i in range(10):
         coor_x.append(1)
