04bf4f3a80b4aa5906254f53e71d95c87751bc3b
jakejack13 <jckawesome13@gmail.com>
2018-12-03 17:15:56 -0500

    Attempt to add minimax

--- KerrJacobTicTacToe.py
+++ KerrJacobTicTacToe.py
@@ -58,7 +58,67 @@ def checkFull(input, chosenNumber):
             return True
     return False
 
+hPlayer = "O"
+cPlayer = "X"
 
+#Returns list of empty spaces
+def emptyIndexes(board) :
+    newBoard = []
+    for i in board :
+        if i != "O" and i != "X" :
+            newBoard.append(i)
+    return newBoard
+
+#Checks win conditions
+def checkWin(board, player) :
+    if (board[0] == player and board[1] == player and board[2] == player) or (board[3] == player and board[4] == player and board[5] == player) or (board[6] == player and board[7] == player and board[8] == player) or (board[0] == player and board[3] == player and board[6] == player) or (board[1] == player and board[4] == player and board[7] == player) or (board[2] == player and board[5] == player and board[8] == player) or (board[0] == player and board[4] == player and board[8] == player) or (board[2] == player and board[4] == player and board[6] == player) :
+        return True
+    else:
+        return False
+
+#Minimax algorithm
+def minMax(newBoard, player) :
+    availSpots = emptyIndexes(newBoard)
+    #Checking win conditions and returning score if met
+    if checkWin(newBoard,hPlayer) :
+        return {"score":-10}
+    elif checkWin(newBoard, cPlayer) :
+        return {"score":10}
+    elif len(availSpots) == 0 :
+        return {"score":0}
+    moves = []
+    #Looping through possible moves in each space
+    for i in range(len(availSpots)) :
+        move = {
+            "index":None,
+            "score":None
+        }
+        move["index"] = newBoard[availSpots[i]]
+        newBoard[availSpots[i]] = player
+        if (player == cPlayer) :
+            #Recursion to check all possible moves
+            result = minMax(newBoard, hPlayer)
+            move["score"] = result["score"]
+        else :
+            result = minMax(newBoard, cPlayer)
+            move["score"] = result["score"]
+        #Replacing lost element and adding potential move to list
+        newBoard[availSpots[i]] = move["index"]
+        moves.append(move)
+    #Checking best possible move from move list
+    if player == cPlayer :
+        bestScore = -10000
+        for i in range(len(moves)) :
+            if moves[i]["score"] > bestScore :
+                bestScore = moves[i]["score"]
+                bestMove = i
+    else :
+        bestScore = 10000
+        for i in range(len(moves)) :
+            if moves[i]["score"] < bestScore :
+                bestScore = moves[i]["score"]
+                bestMove = i
+    return moves[bestMove]
 
 def winningCheck(input):
     simNums = 0
@@ -80,13 +140,15 @@ while(play_state == 1):
         #Player Message and Turn Selection
         if player_turn == 0 :
                 print("Player X's turn")
+                turn_selection = int(input("Please select the number of the square to play your tile: "))
         elif player_turn == 1 :
                 print("Player O's turn")
+                turn_selection = minMax();
         else:
                 print("Error! Shutting down program!")
                 break
         #Square Selection
-        turn_selection = int(input("Please select the number of the square to play your tile: "))
+
         #Revise square state system
 
 
@@ -122,4 +184,4 @@ while(play_state == 1):
                         player_turn = 0
                 else :
                         print("Error! Shutting down program!")
-#Hello world
+#Hell world
